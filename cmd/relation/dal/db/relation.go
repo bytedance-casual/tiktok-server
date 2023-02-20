a3bb2eca09cf8998a4bf506203b7fe11f9fe2d06
xixiwang <fewzh@mail.scut.edu.cn>
2023-02-20 18:36:02 +0800

    feature: 关注接口

--- cmd/relation/dal/db/relation.go
+++ cmd/relation/dal/db/relation.go
@@ -2,22 +2,22 @@ package db
 
 import (
 	"context"
-	//"github.com/cloudwego/kitex-examples/bizdemo/easy_note/pkg/constants"
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
 )
 
-type FollowStatus uint8
+type FollowStatus bool
 
 const (
-	Nolmal   = true
-	Canceled = false
+	Normal   FollowStatus = true
+	Canceled FollowStatus = false
 )
 
 type Follow struct {
 	gorm.Model
-	UserId         string `json:"user_id"`
-	FollowedUserId string `json:"followed_user_id"`
-	IsFollow       int    `json:"is_follow"`
+	UserId         string       `json:"user_id"`
+	FollowedUserId string       `json:"followed_user_id"`
+	IsFollow       FollowStatus `json:"is_follow"`
 }
 
 type UserInfoResp struct {
@@ -32,14 +32,39 @@ func (f *Follow) TableName() string {
 	return "follows"
 }
 
-// 查询粉丝列表 -连表查询 -返回粉丝信息
+func CreateFollow(ctx context.Context, actor string, toUser string) (err error) {
+	err = DB.WithContext(ctx).Create(Follow{
+		UserId:         actor,
+		FollowedUserId: toUser,
+		IsFollow:       Normal,
+	}).Error
+	return
+}
+
+func UpdateFollow(ctx context.Context, id uint, status bool) (err error) {
+	err = DB.WithContext(ctx).Where("id", id).Update("is_follow", status).Error
+	return
+}
+
+func QueryHasFollow(ctx context.Context, actor string, toUser string) (f *Follow, err error) {
+	err = DB.WithContext(ctx).Where(map[string]string{
+		"user_id":          actor,
+		"followed_user_id": toUser,
+	}).First(f).Error
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return
+}
+
+// QueryFollowerList 查询粉丝列表 -连表查询 -返回粉丝信息
 func QueryFollowerList(ctx context.Context, userID int64) ([]*UserInfoResp, error) {
 	res := make([]*UserInfoResp, 0)
 	if userID == 0 {
 		return res, nil
 	}
 
-	if err := DB.WithContext(ctx).Table("users").Select("users.id, users.username, users.follow_count, users.follower_count, follows.is_follow").Joins("join follows on follows.user_id = users.id").Where("follows.followed_user_id = ? AND follows.is_follow = ?", userID, Nolmal).Find(&res).Error; err != nil {
+	if err := DB.WithContext(ctx).Table("users").Select("users.id, users.username, users.follow_count, users.follower_count, follows.is_follow").Joins("join follows on follows.user_id = users.id").Where("follows.followed_user_id = ? AND follows.is_follow = ?", userID, Normal).Find(&res).Error; err != nil {
 		return nil, err
 	}
 	/*
@@ -49,7 +74,7 @@ func QueryFollowerList(ctx context.Context, userID int64) ([]*UserInfoResp, erro
 	return res, nil
 }
 
-// 查询关注列表--连表查询 -返回偶像信息
+// QueryFollowList 查询关注列表--连表查询 -返回偶像信息
 func QueryFollowList(ctx context.Context, userID int64) ([]*UserInfoResp, error) {
 	res := make([]*UserInfoResp, 0)
 
@@ -57,7 +82,7 @@ func QueryFollowList(ctx context.Context, userID int64) ([]*UserInfoResp, error)
 		return res, nil
 	}
 
-	if err := DB.WithContext(ctx).Table("users").Select("users.id, users.username, users.follow_count, users.follower_count, follows.is_follow").Joins("join follows on follows.followed_user_id = users.id").Where("follows.user_id = ? AND follows.is_follow = ?", userID, Nolmal).Find(&res).Error; err != nil {
+	if err := DB.WithContext(ctx).Table("users").Select("users.id, users.username, users.follow_count, users.follower_count, follows.is_follow").Joins("join follows on follows.followed_user_id = users.id").Where("follows.user_id = ? AND follows.is_follow = ?", userID, Normal).Find(&res).Error; err != nil {
 		return nil, err
 	}
 	/*
--- cmd/relation/service/action_relation.go
+++ cmd/relation/service/action_relation.go
@@ -1,1 +1,51 @@
 package service
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"strconv"
+	"tiktok-server/cmd/relation/dal/db"
+	"tiktok-server/internal/middleware"
+	"tiktok-server/kitex_gen/relation"
+)
+
+type ActionRelationService struct {
+	ctx context.Context
+}
+
+func NewActionRelationService(ctx context.Context) *ActionRelationService {
+	return &ActionRelationService{ctx: ctx}
+}
+
+func (s *ActionRelationService) Follow(req *relation.RelationActionRequest) (err error) {
+	claims, err := middleware.ParseToken(req.Token)
+	if err != nil {
+		return err
+	}
+	actor := strconv.FormatInt(claims.ID, 10)
+	toUser := strconv.FormatInt(req.ToUserId, 10)
+
+	follow, err := db.QueryHasFollow(s.ctx, actor, toUser)
+	if err != nil {
+		return err
+	}
+
+	switch req.ActionType {
+	case 1: //关注
+		if follow == nil {
+			return db.CreateFollow(s.ctx, actor, toUser)
+		}
+		if follow.IsFollow {
+			return errors.New("你已经关注该用户")
+		}
+		err = db.UpdateFollow(s.ctx, follow.ID, true)
+	case 2: //取消关注
+		if follow == nil || follow.IsFollow == db.Canceled {
+			return errors.New("未关注该用户")
+		}
+		err = db.UpdateFollow(s.ctx, follow.ID, false)
+	default:
+		err = errors.New("不支持的操作")
+	}
+	return
+}
